074b560142ff7ec1cbf3550d780eded45ceae031
David <cuishengxi@189.cn>
2018-04-09 09:29:27 +0800

    银行卡自动输入识别

---
+++ 银行卡/银行卡/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  银行卡
+//
+//  Created by 丁小山 on 15/11/26.
+//  Copyright © 2015年 丁小山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 银行卡/银行卡/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  银行卡
+//
+//  Created by 丁小山 on 15/11/26.
+//  Copyright © 2015年 丁小山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
